5d1fefed1e52850fd1581c0d791148207aa491b3
pancho <pancho@localhost.localdomain>
2020-09-25 17:26:45 -0400

    setup: yum -y

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,12 +2,12 @@
 # este script es para establecer el entorno de trabajo, solo para centos 7
 
 # instalacion de dependencias
-yum install epel-release
-yum install fontconfig-devel gcc-c++ expat-devel python-pyside-devel shiboken-devel qt-devel boost169-devel pixman-devel cairo-devel
+yum -y install epel-release
+yum -y install fontconfig-devel gcc-c++ expat-devel python-pyside-devel shiboken-devel qt-devel boost169-devel pixman-devel cairo-devel
 # -------------------
 
 # Natron necesita gcc-c++ superior al 4.9, asi que necesitamos el 'devtoolset-8'
-yum install centos-release-scl-rh
+yum -y install centos-release-scl-rh
 yum -y install devtoolset-8
 # -----------------------
 
