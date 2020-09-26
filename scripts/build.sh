6e8abe526aa7b7e22e4dc5ea1bc979e6753967bc
pancho <pancho@localhost.localdomain>
2020-09-25 20:03:56 -0400

    cambio de icono a nitrogeno

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,17 @@ thread=4
 natron_src='./'
 qmake="qmake-qt4"
 
+install_folder='/opt/Natron2'
+
 # entorno para gcc 8.3
 source /opt/rh/devtoolset-8/enable
-# -----------------
+#
 
 cd $natron_src
 
 pkill -9 Nitrogen
 rm App/Nitrogen
-rm /opt/Natron2/bin/Natron
+rm $install_folder/bin/Nitrogen
 
 # Compilar sass stylesheet
 npm run d
@@ -24,7 +26,17 @@ make -j $thread
 # -------------------
 
 # copia Natron compilado a la carpeta de la instalacion de Natron
+cp ./App/Nitrogen $install_folder/bin/Nitrogen
+#
 
-cp ./App/Nitrogen /opt/Natron2/bin/Natron
-/opt/Natron2/bin/Natron
-# -------------------
+# copia stylesheet
+cp ./Gui/Resources/Stylesheets/mainstyle.qss $install_folder/Resources/stylesheets/mainstyle.qss
+
+# Natron desktop
+cp ./Gui/Resources/Applications/Nitrogen $install_folder/Nitrogen
+chmod 755 $install_folder/Nitrogen
+cp ./Gui/Resources/Applications/Nitrogen.desktop /usr/share/applications/Nitrogen.desktop
+
+# icono
+mkdir $install_folder/Resources/Images
+cp ./Gui/Resources/Images/nitrogen.png $install_folder/Resources/Images/nitrogen.png
